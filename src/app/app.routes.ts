68191d3f42f6ccc943927e635717e9c6c8742670
muhia8943 <nmmuhia@gmail.com>
2024-10-02 18:14:25 +0300

    login changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:HomePageComponent},
+    {path: 'home',pathMatch: 'full', redirectTo:''},
+    {path: 'login', component:LoginComponent},
+    {path: 'signup', component:SignUpComponent}
+];
